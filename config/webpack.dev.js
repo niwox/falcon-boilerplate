9f745850170f157993bae11674ce9e5cc6e6aa6a
Akram <a.elyoubi@dailymotion.com>
2018-02-28 13:07:22 +0100

    turn webpack config into a function and test npm scripts with args

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,42 +1,44 @@
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-console.log('cwd', process.cwd())
+console.log('cwd', process.cwd());
 
-module.exports = {
-  entry: path.join(process.cwd(), 'app/index.js'),
-  output: {
-    filename: 'bundle.js',
-    // path: path.join(__dirname, 'build')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // add jsx
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = env => {
+  console.log('env', env);
+
+  return {
+    entry: path.join(process.cwd(), 'app/index.js'),
+    output: {
+      filename: '[chunkhash].bundle.js',
+      // path: path.join(__dirname, 'build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/, // add jsx
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
-    ],
-  },
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [new UglifyJsPlugin()],
+  };
 };
